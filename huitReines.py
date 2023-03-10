8a7017d1bdb7d1e827ff12a92960a3347f04c64b
Alexandre <Alexandre.alm18@gmail.com>
2023-03-10 08:40:37 +0100

    le debut

--- huitReines.py
+++ huitReines.py
@@ -1,4 +1,18 @@
-class Echuiquier :
+from tkinter import *
+import turtle
+class Echiquier :
     def __init__(self):
         self.cote = 8
+        self.Grille = [[' ♛ ' for _ in range(self.cote)] for _ in range(self.cote)]
         
+    def display(self):
+        for i in range(self.cote):
+            
+            for j in range(self.cote):
+                
+                print(self.Grille[i][j],end='')
+            print('')
+                
+myEchiquier = Echiquier()
+
+myEchiquier.display()
