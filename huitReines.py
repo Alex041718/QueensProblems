dd4b4600d634f067b0b46a87a180dc93e2af6fa5
Alexandre <Alexandre.alm18@gmail.com>
2023-03-13 09:57:55 +0100

    ça avance

--- huitReines.py
+++ huitReines.py
@@ -1,9 +1,20 @@
 from tkinter import *
 import turtle
+
+class Queen :
+    def __init__(self, num, row, column):
+        self.num = num
+        self.row = row
+        self.column = column
+        
+
+
+
 class Echiquier :
     def __init__(self):
         self.cote = 8
         self.Grille = [[' _ ' for _ in range(self.cote)] for _ in range(self.cote)]
+        self.compteur = 0
         
     def display(self):
         for i in range(self.cote):
@@ -14,8 +25,23 @@ class Echiquier :
             print('')
             
     def placeQueen(self,i,j):
-        self.Grille[i][j] = ' ♛ '
-                
+        self.Grille[i][j] = Queen(self.compteur,i,j)
+        self.compteur = self.compteur + 1
+        
+    def deplaceQueen(self,i,j):
+        if(j<self.cote-1) : 
+            temp = self.Grille[i][j+1]
+            self.Grille[i][j+1] = self.Grille[i][j]
+            self.Grille[i][j] = temp
+        elif(j==self.cote-1 and i <self.cote-1) :
+            temp = self.Grille[i+1][0]
+            self.Grille[i+1][0] = self.Grille[i][j]
+            self.Grille[i][j] = temp
+
+    def verrif(self):
+        res = True
+        
+             
 myEchiquier = Echiquier()
 myEchiquier.placeQueen(3,3)
 
